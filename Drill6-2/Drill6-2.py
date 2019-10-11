1490059350cd1dcaadaccf5a465338b86a174d81
xhdrkawk1 <55053318+xhdrkawk1@users.noreply.github.com>
2019-10-11 12:00:20 +0900

    Final
    Commit 4
    https://www.youtube.com/watch?v=dFu56uLqP4M
    2016180051

--- Drill6-2/Drill6-2.py
+++ Drill6-2/Drill6-2.py
@@ -95,5 +95,4 @@ PlayerMotion = 1
 while running:
     draw_curve_Random(point[0], point[1], point[2], point[3], point[4], point[5], point[6], point[7], point[8], point[9])
     handle_events()
-
 close_canvas()
