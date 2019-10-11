add46a25dfce7e7805c73104ca67fd797c7b7f57
xhdrkawk1 <55053318+xhdrkawk1@users.noreply.github.com>
2019-10-11 11:53:48 +0900

    2016180051 Driil 6-2 Final
    https://www.youtube.com/watch?v=dFu56uLqP4M
    Commit 4
    2016180051 김희준

--- Drill6-2/Drill6-2.py
+++ Drill6-2/Drill6-2.py
@@ -31,14 +31,6 @@ def move_point(p):
     handle_events()
 
 
-def draw_start_end(p1, p2, p3, near, far):
-    for i in range(near, far, 2):
-        t = i / 100
-        x = (2 * t ** 2 - 3 * t + 1) * p1[0] + (-4 * t ** 2 + 4 * t) * p2[0] + (2 * t ** 2 - t) * p3[0]
-        y = (2 * t ** 2 - 3 * t + 1) * p1[1] + (-4 * t ** 2 + 4 * t) * p2[1] + (2 * t ** 2 - t) * p3[1]
-        move_point((x, y))
-
-
 def draw_Curve(p1, p2, p3, p4, Small, Big):
     direct_character(p2, p3)
 
