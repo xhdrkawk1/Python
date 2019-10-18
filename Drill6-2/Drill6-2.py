fc27b7a718f1f303b247ab6a9a2649045fea7562
xhdrkawk1 <55053318+xhdrkawk1@users.noreply.github.com>
2019-10-18 11:59:51 +0900

    Update Drill6-2.py

--- Drill6-2/Drill6-2.py
+++ Drill6-2/Drill6-2.py
@@ -96,4 +96,8 @@ PlayerMotion = 1
 while running:
     draw_curve_Random(point[0], point[1], point[2], point[3], point[4], point[5], point[6], point[7], point[8], point[9])
     handle_events()
+<<<<<<< HEAD
 close_canvas()
+=======
+close_canvas()
+>>>>>>> d1ff32846fdc7af8314336fc92d9081af37207f4
