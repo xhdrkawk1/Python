17bbbcc88a6f306fd8b76cdc08cc40376925aae6
xhdrkawk1 <55053318+xhdrkawk1@users.noreply.github.com>
2019-10-23 13:01:24 +0900

    Revert "Commit 3"
    This reverts commit 23407c62ae30343d275f21ca317c165f36512367.

--- Drill8/pause_state.py
+++ Drill8/pause_state.py
@@ -4,33 +4,32 @@ import main_state
 
 
 name = "PauseState"
-PauseBotton = None
-
-
-class CPauseButton:
-    def __init__(self):
-        self.image = load_image('pause.png')
-
-    def draw(self):
-        self.image.draw(400, 300)
+image = None
 
 
 def enter():
-   global  PauseBotton
-   PauseBotton = CPauseButton()
+    global image
+    image = load_image('pause.png')
+
 
 def exit():
-   pass
+    global image
+    del(image)
 
 
 def handle_events():
     events = get_events()
     for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        else:
             if (event.type, event.key) == (SDL_KEYDOWN, SDLK_p):
                 game_framework.pop_state()
+
+
 def draw():
     clear_canvas()
-    PauseBotton.draw()
+    image.draw(400, 300)
     update_canvas()
 
 
