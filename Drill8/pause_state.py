23407c62ae30343d275f21ca317c165f36512367
xhdrkawk1 <55053318+xhdrkawk1@users.noreply.github.com>
2019-10-23 13:00:08 +0900

    Commit 3
    https://www.youtube.com/watch?v=mE2Um93Y1K0
    
    Commit3

--- Drill8/pause_state.py
+++ Drill8/pause_state.py
@@ -4,32 +4,33 @@ import main_state
 
 
 name = "PauseState"
-image = None
+PauseBotton = None
+
+
+class CPauseButton:
+    def __init__(self):
+        self.image = load_image('pause.png')
+
+    def draw(self):
+        self.image.draw(400, 300)
 
 
 def enter():
-    global image
-    image = load_image('pause.png')
-
+   global  PauseBotton
+   PauseBotton = CPauseButton()
 
 def exit():
-    global image
-    del(image)
+   pass
 
 
 def handle_events():
     events = get_events()
     for event in events:
-        if event.type == SDL_QUIT:
-            game_framework.quit()
-        else:
             if (event.type, event.key) == (SDL_KEYDOWN, SDLK_p):
                 game_framework.pop_state()
-
-
 def draw():
     clear_canvas()
-    image.draw(400, 300)
+    PauseBotton.draw()
     update_canvas()
 
 
